3769a4c0f248c6cdadf8f7269440dfae462488b2
METEHANBYRKDR <102472877+METEHANBYRKDR@users.noreply.github.com>
2024-03-04 11:02:02 +0300

    CommitRepositoryPattern

--- CleanArchitecture.Persistance/Services/CarService.cs
+++ CleanArchitecture.Persistance/Services/CarService.cs
@@ -1,8 +1,12 @@
 ﻿using AutoMapper;
 using CleanArchitecture.Application.Features.CarFeatures.Commands.CreateCar;
+using CleanArchitecture.Application.Features.CarFeatures.Queries.GetAllCar;
 using CleanArchitecture.Application.Services;
 using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Repositories;
 using CleanArchitecture.Persistance.Context;
+using GenericRepository;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace CleanArchitecture.Persistance.Services;
@@ -11,16 +15,28 @@ public class CarService : ICarService
 {
     private readonly AppDbContext _context;
     private readonly IMapper _mapper;
+    private readonly ICarRepository _carRepository;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public CarService(AppDbContext context,IMapper mapper)
+    public CarService(AppDbContext context, IMapper mapper, ICarRepository carRepository, IUnitOfWork unitOfWork)
     {
         _mapper = mapper;
         _context = context;
+        _carRepository = carRepository;
+        _unitOfWork = unitOfWork;
     }
     public async Task CreateAsync(CreateCarCommand request, CancellationToken cancellationToken)
     {
         Car car = _mapper.Map<Car>(request);
-        await _context.Set<Car>().AddAsync(car, cancellationToken);
-        await _context.SaveChangesAsync(cancellationToken);
+        //await _context.Set<Car>().AddAsync(car, cancellationToken);
+        //await _context.SaveChangesAsync(cancellationToken);
+        await _carRepository.AddAsync(car, cancellationToken);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<IList<Car>> GetAllAsync(GetAllCarQuery request, CancellationToken cancellationToken)
+    {
+        IList<Car> cars = await _carRepository.GetAll().ToListAsync(cancellationToken);
+        return cars;
     }
 }
--- CleanArchitecture.WebApi/Program.cs
+++ CleanArchitecture.WebApi/Program.cs
@@ -1,8 +1,12 @@
 using CleanArchitecture.Application.Behaviors;
 using CleanArchitecture.Application.Services;
+using CleanArchitecture.Domain.Repositories;
 using CleanArchitecture.Persistance.Context;
+using CleanArchitecture.Persistance.Repositories;
 using CleanArchitecture.Persistance.Services;
 using CleanArchitecture.WebApi.Middleware;
+using CleanArchitecture.Domain.Repositories;
+
 using FluentValidation;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +16,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<ICarService, CarService>();
 builder.Services.AddAutoMapper(typeof(CleanArchitecture.Persistance.AssemblyReferance).Assembly);
 builder.Services.AddTransient<ExceptionMiddleware>();
+//builder.Services.AddScoped<IUnitOfWork, UnitOfWork<AppDbContext>>();
+
 
 string connectionString = builder.Configuration.GetConnectionString("SqlServer");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
